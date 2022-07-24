0eb438bf3866b841fbb06c277adef6875ce33261
Anthony Beckford <abeckford532@gmail.com>
2022-07-24 13:51:51 -0400

    Add files via upload
    8 kyu Function 3 - multiplying two numbers
    
    Implement a function which multiplies two numbers.

---
+++ multiplyByTwo/multiplyTwo.js
@@ -0,0 +1,12 @@
+
+// 8 kyu Function 3 - multiplying two numbers
+
+// Implement a function which multiplies two numbers.
+
+// Write here your multiply-function
+function multiply(a, b){
+    let multipleTwo = a * b;
+    return multipleTwo 
+  }
+  
+  multiply(2,2);
---
+++ multiplyByTwo/test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(multiply(2, 3), 6);
+  });
+});
